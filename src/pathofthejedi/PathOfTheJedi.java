fd46dd42bb968eca2921f125d8448e9921e1f586
geoffreywhetstone <geoffreywhetstone@172.16.154.1>
2015-05-14 15:29:48 -0600

    Initial Commit

---
+++ src/pathofthejedi/PathOfTheJedi.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pathofthejedi;
+
+import byui.cit260.jedi.model.Game;
+
+/**
+ *
+ * @author geoffreywhetstone
+ */
+public class PathOfTheJedi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game gamemain = new Game();
+        
+     gamemain.setExploreCurrentLocation("Current");
+     gamemain.setReloadLastSave("Reload Last Saved Game");
+     gamemain.setReturnToShip("Return to Ship");
+     gamemain.setSaveGame("Save Game");
+     gamemain.setViewCrew("View Crew");
+     gamemain.setViewIventory("View Inventory");
+     
+     
+     String gamemenu = gamemain.toString();
+        System.out.println(gamemenu);
+    }
+
+
+    
+    
+    
+}
