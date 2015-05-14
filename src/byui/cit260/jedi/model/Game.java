bfbc82807425726ef315e590ae14f191f380fc73
Moses <Moses@MasterJedi>
2015-05-14 16:00:42 -0600

    My Trainings Class

---
+++ src/byui/cit260/jedi/model/Game.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.jedi.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author geoffreywhetstone
+ */
+public class Game {
+    //class instance variables
+    private String exploreCurrentLocation;
+    private String viewIventory;
+    private String viewCrew;
+    private String saveGame;
+    private String reloadLastSave;
+    private String exitGame;
+    private String returnToShip;
+
+    @Override
+    public String toString() {
+        return "Game{" + "exploreCurrentLocation=" + exploreCurrentLocation + ", viewIventory=" + viewIventory + ", viewCrew=" + viewCrew + ", saveGame=" + saveGame + ", reloadLastSave=" + reloadLastSave + ", exitGame=" + exitGame + ", returnToShip=" + returnToShip + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.exploreCurrentLocation);
+        hash = 41 * hash + Objects.hashCode(this.viewIventory);
+        hash = 41 * hash + Objects.hashCode(this.viewCrew);
+        hash = 41 * hash + Objects.hashCode(this.saveGame);
+        hash = 41 * hash + Objects.hashCode(this.reloadLastSave);
+        hash = 41 * hash + Objects.hashCode(this.exitGame);
+        hash = 41 * hash + Objects.hashCode(this.returnToShip);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.exploreCurrentLocation, other.exploreCurrentLocation)) {
+            return false;
+        }
+        if (!Objects.equals(this.viewIventory, other.viewIventory)) {
+            return false;
+        }
+        if (!Objects.equals(this.viewCrew, other.viewCrew)) {
+            return false;
+        }
+        if (!Objects.equals(this.saveGame, other.saveGame)) {
+            return false;
+        }
+        if (!Objects.equals(this.reloadLastSave, other.reloadLastSave)) {
+            return false;
+        }
+        if (!Objects.equals(this.exitGame, other.exitGame)) {
+            return false;
+        }
+        if (!Objects.equals(this.returnToShip, other.returnToShip)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    public Game() {
+    }
+    
+    
+
+    public String getExploreCurrentLocation() {
+        return exploreCurrentLocation;
+    }
+
+    public void setExploreCurrentLocation(String exploreCurrentLocation) {
+        this.exploreCurrentLocation = exploreCurrentLocation;
+    }
+
+    public String getViewIventory() {
+        return viewIventory;
+    }
+
+    public void setViewIventory(String viewIventory) {
+        this.viewIventory = viewIventory;
+    }
+
+    public String getViewCrew() {
+        return viewCrew;
+    }
+
+    public void setViewCrew(String viewCrew) {
+        this.viewCrew = viewCrew;
+    }
+
+    public String getSaveGame() {
+        return saveGame;
+    }
+
+    public void setSaveGame(String saveGame) {
+        this.saveGame = saveGame;
+    }
+
+    public String getReloadLastSave() {
+        return reloadLastSave;
+    }
+
+    public void setReloadLastSave(String reloadLastSave) {
+        this.reloadLastSave = reloadLastSave;
+    }
+
+    public String getExitGame() {
+        return exitGame;
+    }
+
+    public void setExitGame(String exitGame) {
+        this.exitGame = exitGame;
+    }
+
+    public String getReturnToShip() {
+        return returnToShip;
+    }
+
+    public void setReturnToShip(String returnToShip) {
+        this.returnToShip = returnToShip;
+    }
+    
+    
+}
---
+++ src/byui/cit260/jedi/model/Trainings.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.jedi.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Moses
+ */
+public class Trainings {
+        //class instance variables
+        private String  lightSaberUse;
+        private String combat;
+        private String forcePush;
+        private String forceAffinity;
+        private String returnToShip;
+        private String useHologram;
+
+    @Override
+    public String toString() {
+        return "Trainings{" + "lightSaberUse=" + lightSaberUse + ", combat=" + combat + ", forcePush=" + forcePush + ", forceAffinity=" + forceAffinity + ", returnToShip=" + returnToShip + ", useHologram=" + useHologram + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + Objects.hashCode(this.lightSaberUse);
+        hash = 47 * hash + Objects.hashCode(this.combat);
+        hash = 47 * hash + Objects.hashCode(this.forcePush);
+        hash = 47 * hash + Objects.hashCode(this.forceAffinity);
+        hash = 47 * hash + Objects.hashCode(this.returnToShip);
+        hash = 47 * hash + Objects.hashCode(this.useHologram);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Trainings other = (Trainings) obj;
+        if (!Objects.equals(this.lightSaberUse, other.lightSaberUse)) {
+            return false;
+        }
+        if (!Objects.equals(this.combat, other.combat)) {
+            return false;
+        }
+        if (!Objects.equals(this.forceAffinity, other.forceAffinity)) {
+            return false;
+        }
+        if (!Objects.equals(this.returnToShip, other.returnToShip)) {
+            return false;
+        }
+        if (!Objects.equals(this.useHologram, other.useHologram)) {
+            return false;
+        }
+        return true;
+    }
+        
+        
+
+    public Trainings() {
+    }
+        
+        
+    public String getLightSaberUse() {
+        return lightSaberUse;
+    }
+
+    public void setLightSaberUse(String lightSaberUse) {
+        this.lightSaberUse = lightSaberUse;
+    }
+
+    public String getCombat() {
+        return combat;
+    }
+
+    public void setCombat(String combat) {
+        this.combat = combat;
+    }
+
+    public String getForcePush() {
+        return forcePush;
+    }
+
+    public void setForcePush(String forcePush) {
+        this.forcePush = forcePush;
+    }
+
+    public String getForceAffinity() {
+        return forceAffinity;
+    }
+
+    public void setForceAffinity(String forceAffinity) {
+        this.forceAffinity = forceAffinity;
+    }
+
+    public String getReturnToShip() {
+        return returnToShip;
+    }
+
+    public void setReturnToShip(String returnToShip) {
+        this.returnToShip = returnToShip;
+    }
+
+    public String getUseHologram() {
+        return useHologram;
+    }
+
+    public void setUseHologram(String useHologram) {
+        this.useHologram = useHologram;
+    }
+        
+        
+    
+}
