392ce30dacd7cee0d17eafafd78bf9c3c208b3b3
geoffreywhetstone <geoffreywhetstone@192.168.8.105>
2015-05-28 20:06:21 -0600

    Geoffrey's Individual Assignment

---
+++ src/byui/cit260/jedi/control/LocationControl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.jedi.control;
+
+/**
+ *
+ * @author geoffreywhetstone
+ */
+public class LocationControl {
+  
+    
+    public double combatEnemy(double userHP, double enemyHP) {
+       
+        if(userHP >= enemyHP) { //userHP is more than enemy so user wins
+            double win = 1;
+            return win;
+        } 
+        if(userHP < enemyHP) { //userHP is less than enemy so user loses
+            double lose = -1;
+            return lose;
+        }
+        double currentHP = userHP;
+        double totalHP = currentHP - enemyHP;
+        
+        return totalHP;
+            
+    }
+}
